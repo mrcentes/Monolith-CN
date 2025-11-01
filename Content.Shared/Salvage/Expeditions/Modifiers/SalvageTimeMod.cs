826e6f0941fc2db6be72ecece7ab6416ce25da4e
NXS2333 <148679657+NXS2333@users.noreply.github.com>
2025-11-01 12:22:34 +0800

    延长远征持续时间为一小时

--- Content.Shared/Salvage/Expeditions/Modifiers/SalvageTimeMod.cs
+++ Content.Shared/Salvage/Expeditions/Modifiers/SalvageTimeMod.cs
@@ -18,10 +18,10 @@ public sealed class SalvageTimeMod : IPrototype, ISalvageMod
     public float Cost { get; private set; }
 
     [DataField("minDuration")]
-    public int MinDuration = 900;
+    public int MinDuration = 3600;
 
     [DataField("maxDuration")]
-    public int MaxDuration = 930;
+    public int MaxDuration = 3600;
 
     // Hack: Description isn't nullable
     [DataField]
