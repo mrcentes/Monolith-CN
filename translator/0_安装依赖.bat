c6c8581c1d6ce5a28ac9247abbd9bb66e988733f
Mr.Centes <47635247+mrcentes@users.noreply.github.com>
2026-08-27 16:30:31 +0800

    删除了未完成的汉化文件
    删除了原先未完成的占位文件，新内容待续。

--- translator/0_安装依赖.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001
-echo 正在安装依赖库...
-pip install -r requirements.txt
-echo.
-echo 依赖安装完成！
-pause
--- translator/Start_GUI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001
-echo 正在启动 SS14 汉化工具箱...
-python ss14_tracker.py
-pause
